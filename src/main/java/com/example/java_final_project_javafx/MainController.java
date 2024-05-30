2534372efdde697b4400a678214829d3cc3bf0d6
AnthonyQwO <anthonyaa0423@gmail.com>
2024-05-30 20:28:38 +0800

    Update ignore.

--- src/main/java/com/example/java_final_project_javafx/MainController.java
+++ src/main/java/com/example/java_final_project_javafx/MainController.java
@@ -101,7 +101,7 @@ public class MainController {
         // 輸入完成度
         TextField progressField = new TextField();
         progressField.setText("100");
-        progressField.setPromptText("Completion (0-100%)");
+        progressField.setPromptText("Completion (0 - 100%)");
 
         Button addButton = new Button("Add");
         addButton.setOnAction(event -> handleAddTask(newTaskField, startDatePicker, endDatePicker, progressField, dialog));
