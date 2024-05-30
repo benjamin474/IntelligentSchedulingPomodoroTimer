5c4369381f00f9fc939b8f214d4dfaac0e35e8cc
benjamin474 <coding47414@gmail.com>
2024-05-30 20:50:21 +0800

    Solve Storage Problem

--- src/main/java/com/example/java_final_project_javafx/TaskStorage.java
+++ src/main/java/com/example/java_final_project_javafx/TaskStorage.java
@@ -3,22 +3,16 @@ package com.example.java_final_project_javafx;
 import javafx.collections.ObservableList;
 
 import java.io.*;
+import java.nio.Buffer;
+import java.time.LocalDate;
 import java.util.List;
 
 public class TaskStorage {
 
-    private static final String TASKS_FILE = "Tasks.txt";
+    private static final String TASKS_FILE = "Tasks.csv";
 
     public void saveTasksToFile(List<Task> tasks) {
 //       儲存任務資料
-//       try (BufferedWriter writer = new BufferedWriter(new FileWriter(TASKS_FILE))){
-//           for(Task task : tasks) {
-//               writer.write(task);
-//               writer.newLine();
-//           }
-//       }catch(IOException e){
-//           e.printStackTrace();
-//       }
 //        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(TASKS_FILE))) {
 //            out.writeObject(tasks);
 //            System.out.println("Saving tasks to file: " + new File(TASKS_FILE).getAbsolutePath());
@@ -26,6 +20,14 @@ public class TaskStorage {
 //            System.err.println("Error saving tasks to file: " + e.getMessage());
 ////            e.printStackTrace();
 //        }
+        try (PrintWriter writer = new PrintWriter(new File(TASKS_FILE))) {
+            for (Task task : tasks) {
+                writer.println(task.getName() + "," + task.getStartDate() + "," + task.getEndDate() + "," + task.getCompleted());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -33,11 +35,28 @@ public class TaskStorage {
         File file = new File(TASKS_FILE);
         System.out.println("Loading tasks from file: " + file.getAbsolutePath());
 
-        if (file.exists()) {
-            if(file.length() == 0) {
-                System.out.println("The File is empty");
-                return;
+        try (BufferedReader reader = new BufferedReader(new FileReader(TASKS_FILE))) {
+            String line;
+            tasks.clear();
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                String name = fields[0];
+                LocalDate startDate = LocalDate.parse(fields[1]);
+                LocalDate endDate = LocalDate.parse(fields[2]);
+                Integer completed = Integer.parseInt(fields[3]);
+                Task task = new Task(name, startDate, endDate, completed);
+                tasks.add(task);
             }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        if (file.exists()) {
+//            if (file.length() == 0) {
+//                System.out.println("The File is empty");
+//                return;
+//            }
 //            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
 //                List<Task> loadedTasks = (List<Task>) in.readObject();
 //                tasks.addAll(loadedTasks);
@@ -49,6 +68,6 @@ public class TaskStorage {
 //
 //
 //            }
-        }
+//        }
     }
 }
