7fedce18ac63c1b750664a3a62ed39ca475c3c19
benjamin474 <coding47414@gmail.com>
2024-05-31 21:07:49 +0800

    創建class 但先不改MainController

--- src/main/java/com/example/java_final_project_javafx/AddAction.java
+++ src/main/java/com/example/java_final_project_javafx/AddAction.java
@@ -1,4 +1,4 @@
-package com.example.java_final_project_javafx;
-
-public class AddAction extends Action {
-}
+//package com.example.java_final_project_javafx;
+//
+//public class AddAction extends Action {
+//}
